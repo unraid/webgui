c2c0f4d0d70f0b736d197defdb9d054b6b500dd6
Squidly271 <unraidsquid@gmail.com>
2024-03-11 20:03:38 -0400

    Update Wrappers.php

--- emhttp/plugins/dynamix/include/Wrappers.php
+++ emhttp/plugins/dynamix/include/Wrappers.php
@@ -151,11 +151,16 @@ function my_logger($message, $logger='webgui') {
 }
 if ( ! function_exists("file_put_contents_atomic") ) {
   function file_put_contents_atomic($filename, $data, $flags = 0, $context = null) {
-    if (file_put_contents($filename."~", $data, $flags, $context) === strlen($data)) {
-      return rename($filename."~",$filename,$context) ? strlen($data) : false;  
+    $suffix = rand();
+    if ( $flags || $context) 
+      $copyResult = @copy($filename,"$filename$suffix");
+    if ( $copyResult ) {
+      if (file_put_contents("$filename$suffix"), $data, $flags, $context) === strlen($data)) {
+        return rename("$filename$suffix",$filename,$context) ? strlen($data) : false;  
+      }
     }
-    exec("logger ".escapeshellarg("Failed to write $filename"));
-    @unlink($filename."~", $context);
+    my_logger("Failed to write $filename");
+    @unlink("$filename$suffix");
     return false;
   }
 }
