af435395cff50c3811311462a2213602f6d6a0b8
SimonFair <39065407+SimonFair@users.noreply.github.com>
2025-06-06 20:19:36 +0100

    Fix VM Private Networks

--- emhttp/plugins/dynamix.vm.manager/include/libvirt_helpers.php
+++ emhttp/plugins/dynamix.vm.manager/include/libvirt_helpers.php
@@ -1230,7 +1230,7 @@ class Array2XML {
 	function getValidNetworks() {
 		global $lv,$libvirt_running;
 		$arrValidNetworks = [];
-		exec("ls --indicator-style=none /sys/class/net | grep -Po '^(br|bond|eth|wlan)[0-9]+(\.[0-9]+)?'",$arrBridges);
+		exec("ls --indicator-style=none /sys/class/net | grep -Po '^((vir)?br|bond|eth|wlan)[0-9]+(\.[0-9]+)?'",$arrBridges);
 		// add 'virbr0' as default first choice
 		array_unshift($arrBridges, 'virbr0');
 		// remove redundant references of bridge and bond interfaces
